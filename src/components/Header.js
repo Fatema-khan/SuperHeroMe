955bbbbad3a067236ed9e8e27ee37ba806485f93
Fatema-khan <99400186+Fatema-khan@users.noreply.github.com>
2022-07-13 14:34:26 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,17 +9,5 @@ Header.defaultProps={
     title:'',
 }
 
-//css in Js
-//const headingStyle = {
-//    color: 'red',
-//    backgroundColor: 'black'
-//}
-  //<form>
-//     <label>Enter 1st Number:
-  //      <input type="text" />
-    //  </label>
-    //<label>Enter 2nd Number:
-    //<input type="text" />
-  //</label>
-//</form>
+
 export default Header
